cecf7c1a6befad829f36ca3c65ae8309ee7a6610
Pharmasage <mwaqarahmed5566@gmail.com>
2026-07-28 19:41:27 +0500

    videos issue resolved

--- lib/features/videos/presentation/videos_screen.dart
+++ lib/features/videos/presentation/videos_screen.dart
@@ -1,15 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../../config/app_colors.dart';
+import 'local_widgets/reels_page_view.dart';
+import 'providers/videos_provider.dart';
 
-class VideosScreen extends StatelessWidget {
+class VideosScreen extends ConsumerWidget {
   const VideosScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final videosAsync = ref.watch(videosProvider);
+
     return Scaffold(
-      backgroundColor: AppColors.white,
-      body: Center(
-        child: Text('Videos', style: Theme.of(context).textTheme.titleLarge),
+      backgroundColor: AppColors.black,
+      body: videosAsync.when(
+        data: (videos) => ReelsPageView(videos: videos),
+        loading: () => const Center(child: CircularProgressIndicator(color: AppColors.white)),
+        error: (error, stackTrace) => Center(
+          child: Text(
+            'Something went wrong',
+            style: Theme.of(context).textTheme.bodyMedium?.copyWith(color: AppColors.white),
+          ),
+        ),
       ),
     );
   }
